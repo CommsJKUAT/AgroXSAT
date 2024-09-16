e58f7022b062278416e7e33258c0d424be69c253
franfreezy <frandelwanjawa@yahoo.com>
2024-09-16 11:13:13 +0300

    Migrations have finally worked. tip: comment admin in the url and contrib.admin

--- AgritechSat/AgritechSat/settings.py
+++ AgritechSat/AgritechSat/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'PORT': os.environ.get('PORT') ,       
     }
 }
-
+AUTH_USER_MODEL = 'Backend.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
