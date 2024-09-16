a8996d5a04df904b0f79523b671795d7b58a5e61
franfreezy <frandelwanjawa@yahoo.com>
2024-09-16 10:22:22 +0300

    Installed apps updated

--- AgritechSat/AgritechSat/settings.py
+++ AgritechSat/AgritechSat/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Backend.BackendConfig.apps',
+    'BackendApi.BackendapiConfig.apps',
+
 ]
 
 MIDDLEWARE = [
