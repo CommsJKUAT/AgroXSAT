6e26e077a855ea7322fc77fc4d2a1d76e5cbd45e
franfreezy <frandelwanjawa@yahoo.com>
2024-09-19 13:55:56 +0300

    Fixing db issues

--- AgritechSat/AgritechSat/settings.py
+++ AgritechSat/AgritechSat/settings.py
@@ -72,10 +72,10 @@ WSGI_APPLICATION = 'AgritechSat.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('NAME'), 
-        'USER': os.environ.get('USER') ,
-        'PASSWORD': os.environ.get('PASSWORD') ,
-        'HOST': os.environ.get('HOST') ,  
+        #'NAME': os.environ.get('NAME'), 
+        #'USER': os.environ.get('USER') ,
+        #'PASSWORD': os.environ.get('PASSWORD') ,
+        #'HOST': os.environ.get('HOST') ,  
         'PORT': os.environ.get('PORT') ,       
     }
 }
