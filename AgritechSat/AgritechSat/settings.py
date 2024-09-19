fd6cce99d66e2e4d2277affc4343f0c5a7bd0579
franfreezy <frandelwanjawa@yahoo.com>
2024-09-19 17:06:16 +0300

    Update port

--- AgritechSat/AgritechSat/settings.py
+++ AgritechSat/AgritechSat/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         #'USER': os.environ.get('USER') ,
         #'PASSWORD': os.environ.get('PASSWORD') ,
         #'HOST': os.environ.get('HOST') ,  
-        'PORT': os.environ.get('PORT') ,       
+        'PORT': 5432,       
     }
 }
 DATABASES['default'] = dj_database_url.parse(os.getenv("DATABASECREDS"))
