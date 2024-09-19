2c870375968b448ad7c18cdaa022936f3514dfa4
franfreezy <frandelwanjawa@yahoo.com>
2024-09-19 19:32:47 +0300

    Roll back

--- AgritechSat/BackendApi/views.py
+++ AgritechSat/BackendApi/views.py
@@ -4,34 +4,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
-from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
+
 
 def homepage(request):
     return HttpResponse("Agrosat Backend Apis!")
 
-@csrf_exempt
+@api_view(['POST'])
 def backendapires(request):
-    # Check if the request is POST and content-type is application/json
-    if request.method == 'POST' and request.content_type == 'application/json':
-        try:
-            # Parse the JSON data from the request body
-            data = JSONParser().parse(request)
-        except Exception as e:
-            return JsonResponse({'message': 'Error parsing JSON data', 'error': str(e)}, status=400)
-
-        # Now use the parsed data
-        serializer = UserSerializer(data=data)
-
-        if serializer.is_valid():
-            return JsonResponse({
-                'message': 'Data received successfully!',
-                'data': serializer.validated_data
-            }, status=201)
-        else:
-            return JsonResponse({
-                'message': 'Invalid data',
-                'errors': serializer.errors
-            }, status=400)
+    serializer = UserSerializer(data=request.data)
+    print(request.data)
+    if serializer.is_valid():
+        # Process data (e.g., save to database)
+        return Response({
+            'message': 'Data received successfully!',
+            'data': serializer.validated_data
+        }, status=status.HTTP_201_CREATED)
     else:
-        return JsonResponse({'message': 'Invalid request method or content type'}, status=400)
+        return Response({
+            'message': 'Invalid data',
+            'errors': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
