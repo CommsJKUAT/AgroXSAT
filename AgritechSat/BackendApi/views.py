704acc06a944a9d706a14e2782c37323d302e43e
franfreezy <frandelwanjawa@yahoo.com>
2024-09-20 23:02:29 +0300

    This should work

--- AgritechSat/BackendApi/views.py
+++ AgritechSat/BackendApi/views.py
@@ -22,9 +22,9 @@ class backendapires(APIView):
             data_json = json.dumps(data)
             data_json = data_json.get('_content')  # Access the content from the QueryDict
             data_json = data_json[0].replace("\r\n", "")
-            data_json =json.loads(data_json )
-            temperature = data_json.get('temperature')
-            humidity = data_json.get('humidity')
+            data =json.loads(data_json )
+            temperature = data.get('temperature')
+            humidity = data.get('humidity')
 
             # Simple validation check
             if temperature is None or humidity is None:
