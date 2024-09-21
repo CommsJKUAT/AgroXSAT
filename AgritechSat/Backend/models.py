bcb3b6125a4058ac4fe1e6d74b9d8d0cb1c3fd7d
franfreezy <frandelwanjawa@yahoo.com>
2024-09-21 17:18:09 +0300

    Fixing null value in database

--- AgritechSat/Backend/models.py
+++ AgritechSat/Backend/models.py
@@ -12,7 +12,7 @@ class CustomUser(AbstractUser):
 class SensorData(models.Model):
     temperature = models.FloatField()
     humidity = models.FloatField()
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Temp: {self.temperature}, Humidity: {self.humidity}, Time: {self.timestamp}"
