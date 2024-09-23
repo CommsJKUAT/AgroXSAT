354698e1826af9f524c01d2ae4bae0eae3c7a033
franfreezy <frandelwanjawa@yahoo.com>
2024-09-23 18:35:06 +0300

    fix login link

--- AgritechSat/Backend/urls.py
+++ AgritechSat/Backend/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('distance/', calculate_distance, name="homepage"),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('login', views.login),
+    path('login/', views.login),
     
     
 ]
