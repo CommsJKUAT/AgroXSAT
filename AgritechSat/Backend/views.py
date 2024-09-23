64c86021698745a85580e787232ab1ca1951ca7d
franfreezy <frandelwanjawa@yahoo.com>
2024-09-23 17:29:03 +0300

    views for l;ogin and register, leveraging on simple jwt tokens

--- AgritechSat/Backend/views.py
+++ AgritechSat/Backend/views.py
@@ -2,26 +2,60 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse,JsonResponse
 from django.contrib.auth import login, authenticate
 from .forms import CustomUserCreationForm
-
-
-
-
-
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home.html')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'register.html', {'form': form})
-
 from django.shortcuts import render
 from .models import SensorData
 from .forms import CoordinateForm
 from geopy.distance import geodesic
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login(request):
+    serializer = MyTokenObtainPairSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.user
+        token = MyTokenObtainPairSerializer.get_token(user)
+        refresh = token['refresh']
+        access = token['access']
+        return Response({
+            'refresh': str(refresh),
+            'access': str(access),
+            'user': {
+                'username': user.username,
+                'email': user.email
+            }
+        }, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def sensor_data_view(request):
     sensor_data = SensorData.objects.all()  # Fetch all records from the database
