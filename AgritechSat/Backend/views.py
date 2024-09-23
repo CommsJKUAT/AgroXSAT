c39788b5b6a31b20e8461474ec257b1aa4b82a84
franfreezy <frandelwanjawa@yahoo.com>
2024-09-23 17:54:38 +0300

    Finally free to fly and conquer

--- AgritechSat/Backend/views.py
+++ AgritechSat/Backend/views.py
@@ -11,7 +11,8 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
