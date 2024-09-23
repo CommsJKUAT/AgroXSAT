eb57081f816fe47039acc41bf21b9dfbe86aecec
franfreezy <frandelwanjawa@yahoo.com>
2024-09-23 17:31:06 +0300

    urls configured for the backend

--- AgritechSat/Backend/urls.py
+++ AgritechSat/Backend/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
+from . import views
 from .views import sensor_data_view,register,calculate_distance
+from .views import MyTokenObtainPairView
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('', sensor_data_view, name="homepage"),
-    path('register/', register, name="homepage"),
+    path('register/', register, name="register"),
     path('distance/', calculate_distance, name="homepage"),
-    
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login', views.login),
     
     
 ]
