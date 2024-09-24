df26d541c034a37756cf4d4db21dae8197aca653
cosmascodes <75983101+Gacalow@users.noreply.github.com>
2024-09-24 19:16:08 +0300

    Updates on Register page

--- AgritechSat/frontend/src/pages/@auth/register/register.jsx
+++ AgritechSat/frontend/src/pages/@auth/register/register.jsx
@@ -6,7 +6,7 @@ function Register() {
       <Nav />
       <div className="pb-8 flex flex-col justify-center items-center h-full">
         <div className="w-1/3 2xl:w-1/4">
-          <h1 className="text-5xl mb-14 font-semibold text-left text-white">
+          <h1 className="text-4xl 2xl:text-5xl mb-2 2xl:mb-14 font-semibold text-left text-white">
             Sign Up
           </h1>
         </div>
@@ -26,10 +26,21 @@ function Register() {
                 placeholder="John Doe"
               />
               <div class="absolute inset-y-0 end-0 flex items-center pe-3.5 pointer-events-none">
-              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-  <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
-</svg>
-
+                <svg
+                  class="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
               </div>
             </div>
           </div>
