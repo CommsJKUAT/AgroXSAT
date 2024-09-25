9f47e6c243c26c504dc6e1fdd98219465f71295e
franfreezy <frandelwanjawa@yahoo.com>
2024-09-26 00:50:33 +0300

    Bugs bugs bugs

--- AgritechSat/Backend/admin.py
+++ AgritechSat/Backend/admin.py
@@ -1,18 +1,7 @@
-# admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-#from .models import CustomUser#
+from django.contrib.auth.models import User
 
-##class CustomUserAdmin(UserAdmin):
-##    model = CustomUser
-##    # Add fields you want to display in the admin list view
-##    list_display = ('username', 'email', 'phone_number', 'employee_id', 'is_staff', 'is_active')
-##    
-##    fieldsets = UserAdmin.fieldsets + (
-##        (None, {'fields': ('phone_number', 'employee_id')}),
-##    )
-##    add_fieldsets = UserAdmin.add_fieldsets + (
-##        (None, {'fields': ('phone_number', 'employee_id')}),
-##    )
-#
-#admin.site.register(CustomUser, CustomUserAdmin)
+# Register the default User model with the default UserAdmin
+admin.site.unregister(User)  # Unregister the default User admin
+admin.site.register(User, UserAdmin)  # Register the User model with UserAdmin
