c3378366464cdf20ab57d60f2fad81a40cda7a98
franfreezy <frandelwanjawa@yahoo.com>
2024-09-25 22:36:17 +0300

    Forgot some imports

--- AgritechSat/Backend/views.py
+++ AgritechSat/Backend/views.py
@@ -12,6 +12,10 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated,AllowAny
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
