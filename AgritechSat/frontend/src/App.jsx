b0407f755abb5055f9d4f3794a7c2fcf7ea47478
franfreezy <frandelwanjawa@yahoo.com>
2024-09-27 16:00:48 +0300

    Fix home route

--- AgritechSat/frontend/src/App.jsx
+++ AgritechSat/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/@auth/login/login"
 import Register from "./pages/@auth/register/register"
-
+import Home from "./pages/@auth/login/login"
 
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
