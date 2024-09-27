cad2033caaad7abc3ed2b2cef575511caa2d5b50
franfreezy <frandelwanjawa@yahoo.com>
2024-09-27 14:03:31 +0300

    Fix login

--- AgritechSat/frontend/vite.config.js
+++ AgritechSat/frontend/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend/',
   server: {
+      
       proxy: {
           '/backend': {
               target: 'https://agroxsat.onrender.com',
