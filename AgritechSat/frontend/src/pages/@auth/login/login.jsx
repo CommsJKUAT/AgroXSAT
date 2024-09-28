8611cfaa8cf6c64006aa3b40068ee1308a36f916
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-09-28 11:06:39 +0300

    Push

--- AgritechSat/frontend/src/pages/@auth/login/login.jsx
+++ AgritechSat/frontend/src/pages/@auth/login/login.jsx
@@ -1,104 +1,165 @@
 import Nav from "../nav";
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getUser, loginWithGoogle } from "../Auth";
 
 const Login = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
+  const [user, setUser] = useState(null);
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
+  };
 
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log('Form Submitted');
-
-        try {
-            console.log('Sending Request');
-            const response = await fetch('https://agroxsat.onrender.com/backend/login/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData)
-            });
-
-            console.log('Response Status:', response.status);
-
-            const contentType = response.headers.get('Content-Type');
-            const responseBody = await response.text();
-
-            if (response.ok) {
-                const { access, refresh } = JSON.parse(responseBody);
-                localStorage.setItem('accessToken', access);
-                localStorage.setItem('refreshToken', refresh);
-                navigate('/home'); // Redirect to homepage
-            } else {
-                let errorMessage = 'Error logging in';
-
-                if (contentType && contentType.includes('application/json')) {
-                    const result = JSON.parse(responseBody);
-                    errorMessage = result.error || errorMessage;
-                }
-
-                setError(errorMessage);
-            }
-        } catch (err) {
-            console.error('Fetch Error:', err);
-            setError('Error logging in');
+    try {
+      console.log("Sending Request");
+      const response = await fetch(
+        "https://agroxsat.onrender.com/backend/login/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: formData.username,
+            password: formData.password,
+          }),
         }
+      );
+
+      console.log("Response Status:", response.status);
+
+      const contentType = response.headers.get("Content-Type");
+      const responseBody = await response.text();
+
+      if (response.ok) {
+        const { access, refresh } = JSON.parse(responseBody);
+        localStorage.setItem("accessToken", access);
+        localStorage.setItem("refreshToken", refresh);
+        navigate("/home"); // Redirect to homepage
+      } else {
+        let errorMessage = "Error logging in";
+
+        if (contentType && contentType.includes("application/json")) {
+          const result = JSON.parse(responseBody);
+          errorMessage = result.error || errorMessage;
+        }
+
+        setError(errorMessage);
+      }
+    } catch (err) {
+      console.error("Fetch Error:", err);
+      setError("Error logging in");
+    }
+  };
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const userData = await getUser();
+        setUser(userData);
+        if (user !== null) {
+          navigate("/home");
+        }
+      } catch (error) {
+        setUser(null);
+      }
     };
 
-    return (
-        <section className="relative bg-olive 2xl:h-screen bg-hero-pattern bg-no-repeat bg-cover">
-            <Nav />
-            <div className="px-8 md:px-0 pb-8 flex flex-col justify-center items-center h-full">
-                <div className="w-full md:w-1/3 2xl:w-1/4">
-                    <h1 className="text-5xl mb-14 font-semibold text-left text-white">
-                        Sign In
-                    </h1>
-                </div>
-                <form className="w-full md:w-1/3 2xl:w-1/4" onSubmit={handleSubmit}>
-                    <label htmlFor="username" className="block mb-2 text-sm font-medium text-white">
-                        Your Username
-                    </label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={formData.username}
-                        onChange={handleChange}
-                        className="text-white bg-black-olive text-sm rounded-lg focus:ring-black-olive focus:border-black-olive block w-full ps-2.5 p-3.5 placeholder-ash-gray"
-                        placeholder="John Doe"
-                        required
-                    />
-                    <div className="my-5">
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">
-                            Your Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            className="text-white bg-black-olive text-sm rounded-lg focus:ring-black-olive focus:border-black-olive block w-full ps-2.5 p-3.5 placeholder-ash-gray"
-                            placeholder="•••••••••"
-                            required
-                        />
-                        
-                    </div>
-                    <div class="flex items-start mb-5">
+    checkUser();
+  }, []);
+  return (
+    <section className="relative bg-olive 2xl:h-screen bg-hero-pattern bg-no-repeat bg-cover">
+      <Nav />
+      <div className=" px-8 md:px-0 pb-8 flex flex-col justify-center items-center h-full">
+        <div className="w-full md:w-1/3 2xl:w-1/4">
+          <h1 className="text-5xl mb-14 font-semibold text-left text-white">
+            Sign In
+          </h1>
+        </div>
+        <form className="w-full md:w-1/3 2xl:w-1/4 " onSubmit={handleSubmit}>
+          <label
+            htmlFor="username-icon"
+            className="block mb-2 text-sm font-medium text-white"
+          >
+            Your Username
+          </label>
+          <div className="relative">
+            <input
+              type="text"
+              id="username-icon"
+              className="text-white bg-black-olive text-sm rounded-lg focus:ring-black-olive focus:border-black-olive block w-full ps-2.5 p-3.5 placeholder-ash-gray"
+              placeholder="John Doe"
+              required
+            />
+            <div className="absolute inset-y-0 end-0 flex items-center pe-3.5 pointer-events-none">
+              <svg
+                class="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
+          <div className="my-5">
+            <label
+              for="password"
+              className="block mb-2 text-sm font-medium text-white"
+            >
+              Your password
+            </label>
+            <div class="relative">
+              <input
+                type="password"
+                id="password"
+                className="text-white bg-black-olive text-sm rounded-lg focus:ring-black-olive focus:border-black-olive block w-full ps-2.5 p-3.5 placeholder-ash-gray"
+                placeholder="•••••••••"
+                required
+              />
+              <div class="absolute inset-y-0 end-0 flex items-center pe-3.5 pointer-events-none">
+                <svg
+                  class="w-5 h-5 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex items-start mb-5">
             <div className="flex items-center justify-between h-5 w-full">
               <div className="flex items-center ">
                 <input
@@ -119,14 +180,14 @@ const Login = () => {
               </a>
             </div>
           </div>
-                    {error && <div className="mb-4 text-red-500">{error}</div>}
-                    <button
-                        type="submit"
-                        className="mb-4 text-white bg-giants-orange hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-full px-5 py-3.5 text-center"
-                    >
-                        Sign In
-                    </button>
-                    <div className="my-5 flex flex-col">
+          {error && <div className="mb-4 text-red-500">{error}</div>}
+          <button
+            type="submit"
+            className="mb-4 text-white bg-giants-orange hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-full px-5 py-3.5 text-center"
+          >
+            Sign In
+          </button>
+          <div className="my-5 flex flex-col">
             <p className="text-center text-white">Or continue with</p>
             <button
               type="button"
@@ -151,6 +212,7 @@ const Login = () => {
             </button>
             <button
               type="button"
+              onClick={loginWithGoogle}
               class="my-4 text-black-olive bg-white focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-3.5 text-center inline-flex items-center justify-center dark:focus:ring-[#4285F4]/55 me-2 mb-2"
             >
               <svg
@@ -188,10 +250,10 @@ const Login = () => {
               Sign Up
             </a>
           </p>
-                </form>
-            </div>
-        </section>
-    );
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
