c092bc5f6e9a9c719fb26dd33df779719ceacc45
franfreezy <frandelwanjawa@yahoo.com>
2024-09-29 19:44:59 +0300

    Fix imports

--- AgritechSat/BackendApi/views.py
+++ AgritechSat/BackendApi/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
