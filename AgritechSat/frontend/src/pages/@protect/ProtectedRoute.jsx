9ba606351cda5b08429124aec3d0ef214c70aa11
franfreezy <frandelwanjawa@yahoo.com>
2024-09-29 19:52:21 +0300

    Fix

--- AgritechSat/frontend/src/pages/@protect/ProtectedRoute.jsx
+++ AgritechSat/frontend/src/pages/@protect/ProtectedRoute.jsx
@@ -1,19 +1,19 @@
 import { Navigate } from 'react-router-dom';
 
-// Function to check if the user is authenticated
+
 const isAuthenticated = () => {
-  // Check if the accessToken is present in localStorage
+  
   const accessToken = localStorage.getItem('accessToken');
-  return !!accessToken;  // Return true if accessToken exists, false otherwise
+  return !!accessToken;  
 };
 
 const ProtectedRoute = ({ element }) => {
-  // If the user is not authenticated, redirect them to the login page
+  
   if (!isAuthenticated()) {
-    return <Navigate to="/" replace />;  // Redirect to login if not authenticated
+    return <Navigate to="/" replace />;  
   }
 
-  // If authenticated, render the element (protected component)
+  
   return element;
 };
 
