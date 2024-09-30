78e26f36736d7fa4088b1e26d37b031128065865
franfreezy <frandelwanjawa@yahoo.com>
2024-09-30 22:26:20 +0300

    configure location api

--- AgritechSat/BackendApi/views.py
+++ AgritechSat/BackendApi/views.py
@@ -54,3 +54,44 @@ class backendapires(APIView):
         
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@permission_classes([AllowAny])
+class locationapi(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            # Check if request.data is a dictionary (it will be if the content is JSON)
+            if isinstance(request.data, dict) and '_content' not in request.data:
+                data = request.data
+                print("Parsed as JSON:", data)
+            else:
+                # Convert QueryDict to a dictionary
+                data = dict(request.data)
+                print("QueryDict Data:", data)
+                
+                # Extract JSON content from '_content' key
+                data_json = data.get('_content', '')  # Assuming '_content' exists in QueryDict
+                print(data_json)
+                data_json = data_json[0].replace("\r\n", "")  # Clean up new lines if any
+                data = json.loads(data_json)  # Convert JSON string to a Python dictionary
+                print("Extracted Data:", data)
+            
+            #change this to suit the data on esp
+            # Extract location data
+            temperature = data.get('temperature')
+            
+
+            # Simple validation check
+            if temperature is None or humidity is None:
+                return Response({"error": "Missing fields"}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Do something with the data (e.g., save to DB or further processing)
+            #sensor_data = SensorData(
+            #    temperature=temperature,
+            #    humidity=humidity,
+            #    timestamp=timestamp
+            #)
+            #sensor_data.save() We are not saving, we are feeding it to the frontend
+            return Response({"message": "Success", "data": data}, status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
