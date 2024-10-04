f81f814708cf704e2ff150625507721e5ab51a1a
franfreezy <frandelwanjawa@yahoo.com>
2024-10-04 17:00:12 +0300

    configure images url

--- AgritechSat/BackendApi/urls.py
+++ AgritechSat/BackendApi/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import homepage,backendapires,locationapi
+from .views import homepage,backendapires,locationapi,imagesapi
 
 
 urlpatterns = [
     path('', backendapires.as_view(), name="homepage"),
     path('location/', locationapi.as_view(), name="location"),
+    path('images/', imagesapi.as_view(), name="images"),
     
     
 ]
