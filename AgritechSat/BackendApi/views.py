3bb1f8b7eacb226fc31df0c46804d6e431b8fcc8
franfreezy <frandelwanjawa@yahoo.com>
2024-11-11 03:00:29 +0300

    flaws

--- AgritechSat/BackendApi/views.py
+++ AgritechSat/BackendApi/views.py
@@ -299,7 +299,7 @@ def get_gs(request):
         })
 
 def get_location(request):
-    coords = location.objects.order_by('-created_at').first() 
+    coords = location.objects.order_by('-timestamp').first() 
     
     if coords:
         return JsonResponse({
