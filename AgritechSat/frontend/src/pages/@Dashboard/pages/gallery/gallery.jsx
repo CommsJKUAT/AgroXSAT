f41bfaaeb52755ac0e07462dfefd76d6c6fc0bd4
franfreezy <frandelwanjawa@yahoo.com>
2024-11-14 10:38:22 +0300

    upload images

--- AgritechSat/frontend/src/pages/@Dashboard/pages/gallery/gallery.jsx
+++ AgritechSat/frontend/src/pages/@Dashboard/pages/gallery/gallery.jsx
@@ -18,80 +18,40 @@ const Gallery = () => {
           <div>
             <img
               class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+              src="/pictureperfect2.jpg"
               alt=""
             />
           </div>
           <div>
             <img
               class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+              src="/pictureperfect3.jpg"
               alt=""
             />
           </div>
           <div>
             <img
               class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+              src="/pictureperfect4.jpg"
               alt=""
             />
           </div>
           <div>
             <img
               class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+              src="/pictureperfect5.jpg"
               alt=""
             />
           </div>
           <div>
             <img
               class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+              src="/pictureperfect6.jpg"
               alt=""
             />
           </div>
-          <div>
-            <img
-              class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              class="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-              alt=""
-            />
-          </div>
+         
+        
         </div>
       </div>
     </>
