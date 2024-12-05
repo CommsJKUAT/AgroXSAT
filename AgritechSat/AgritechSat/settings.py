7bce5d23b705b7e30eb3203511df75192e8cdfa6
DevFreezy <64470491+franfreezy@users.noreply.github.com>
2024-12-05 22:54:17 +0300

    Update settings.py

--- AgritechSat/AgritechSat/settings.py
+++ AgritechSat/AgritechSat/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    'https://agroxsatsite.onrender.com/',
+    'https://agroxsatsite.onrender.com',
     'http://localhost:5173',
     'http://localhost:4280'
    
